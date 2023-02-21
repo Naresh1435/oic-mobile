7dab52e359814b6d8d0aaf811128d0618cd31f5c
Naresh <naresh45.dev@gmail.com>
2023-02-21 05:55:11 +0530

    redeploy

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import { createBrowserRouter } from "react-router-dom";
 import Home from "./home";
 let App = createBrowserRouter([
--- src/home.jsx
+++ src/home.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ReactDOM from 'react-dom';
 import logo from "./imgs/olympiclogo-1.png"
 import { Link } from "react-router-dom";
 import "./home.css";
